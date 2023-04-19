ce444a62ead9ab7066bf99598dbb4f7abba56618
keowen375@gmail.com <keowen375@gmail.com>
2023-04-19 11:53:49 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+  const [picture, setPicture] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setPicture(data);
+        setLoading(true);
+      });
+  }, []);
+
+  if (!loading) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={picture.message} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
